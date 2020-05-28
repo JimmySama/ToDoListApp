0e99b9fe752b8ae67f8e1aac5f772147b284bcfa
JimmySama <htutwaiphyoe@gmail.com>
2020-05-28 23:26:48 +0630

    Initial

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById('backArrow').addEventListener('click', () => {
+  document.querySelector('.list-form').style.display = 'none';
+});
+document.getElementById('showForm').addEventListener('click', () => {
+  document.querySelector('.list-form').style.display = 'flex';
+});
