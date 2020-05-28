b9e3442ce78c97e21371285a9d86f03682a64e27
JimmySama <htutwaiphyoe@gmail.com>
2020-05-29 00:08:40 +0630

    Initial

--- js/index.js
+++ js/index.js
@@ -4,3 +4,15 @@ document.getElementById('backArrow').addEventListener('click', () => {
 document.getElementById('showForm').addEventListener('click', () => {
   document.querySelector('.list-form').style.display = 'flex';
 });
+document.querySelector('.inbox__icon--completed').addEventListener('click', () => {
+  document.getElementById('completed').style.display = 'flex';
+});
+document.querySelector('.inbox__icon--delete').addEventListener('click', () => {
+  document.getElementById('deleted').style.display = 'flex';
+});
+document.querySelector('.btn-cancel-completed').addEventListener('click', () => {
+  document.getElementById('completed').style.display = 'none';
+});
+document.querySelector('.btn-cancel-delete').addEventListener('click', () => {
+  document.getElementById('deleted').style.display = 'none';
+});
